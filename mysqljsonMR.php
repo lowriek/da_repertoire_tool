0b5e957dd6e80c45580db65c48835c1331c696e2
Kate Lowrie <lowriekate@gmail.com>
2016-10-03 14:01:02 -0400

    Initial version
    SQL query works to create table

---
+++ mysqljsonMR.php
@@ -0,0 +1,17 @@
+<?php // note that this is not a complete page.
+	
+	$dbc = @mysqli_connect("localhost", "root", "root", "da_rep")
+	       or die("Could not open wfb2007 db, " . mysqli_connect_error());
+	$query = "select composer, title from da_performances";				
+
+	$result = mysqli_query($dbc, $query);
+	if ( mysqli_num_rows( $result ) == 0 ) {
+		die("bad query $query");
+	}
+	$performance_data = array();	// put the rows as objects in an array
+	while ( $obj = mysqli_fetch_object( $result ) ) {
+		$performance_data[] = $obj;
+	}
+	echo json_encode($performance_data);
+
+	mysqli_close($dbc);
